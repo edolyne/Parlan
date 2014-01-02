837c7ef709fadce015fb134b3f34bf13dcff862f
Ed Long <elong@impactbnd.com>
2014-01-02 16:57:58 -0500

    End of Day Commit

---
+++ login.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    <
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
+
+
+
+
+
+
+<script src="vex.combined.min.js"></script>
+<script>vex.defaultOptions.className = 'vex-theme-os';</script>
+<link rel="stylesheet" href="vex.css" />
+<link rel="stylesheet" href="vex-theme-os.css" />
+
+
+$(document).ready(function() {  
+  
+        //when button is clicked  
+        $('#check_username_availability').click(function(){  
+            //run the character number check  
+            if($('#username').val() != elon@impactbnd.com ) {  
+                //if it's bellow the minimum show characters_error text '  
+                window.open(http://www.google.com);  
+            }
+        });  
+  
+  });
+            
+
+            
+            
+            
+            window.location.href = "http://new.website.com/that/you/want_to_go_to.html";
